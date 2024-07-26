93c26123e09fc5717ab46bafcbd875d45d747e77
Charlton Tramel <ctramel14@gmail.com>
2024-07-25 20:10:39 -0500

    big ol overhaul

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-const form = document.querySelector('#form')
-const submitButton = document.querySelector('#submit')
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
 
-form.addEventListener('submit', (e) => {
-  submitButton.disabled = true
-  e.preventDefault()
-  window.location.href = window.location.origin + '/success.html'
-})
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
